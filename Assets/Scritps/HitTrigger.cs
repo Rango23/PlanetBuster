81beef1d955992d33656306d75404479df450c85
Rango23 <48034864+Rango23@users.noreply.github.com>
2019-04-02 10:31:10 +1100

    Got Meteor working

---
+++ Assets/Scritps/HitTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class HitTrigger : MonoBehaviour
+{
+    public UnityEvent onTrigger;
+    public bool destroyThing = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(destroyThing)
+        {
+            Destroy(other.gameObject);
+        }
+        // Run the event!
+        onTrigger.Invoke();
+    }
+}
---
+++ Assets/Scritps/MeteorSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSystem : MonoBehaviour
+{
+    public Transform start, end;
+    public Transform meteor;
+    public float movementSpeed = 10f;
+
+    private float percentage = 0;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = Vector3.Lerp(start.position, end.position, percentage);
+        meteor.position = Vector3.MoveTowards(meteor.position, target, movementSpeed * Time.deltaTime);
+    }
+
+    public void MoveCloser(float byPercentage)
+    {
+        percentage += byPercentage;
+    }
+}
