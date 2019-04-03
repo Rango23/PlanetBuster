63f6bdf4d5297ac31921caa9b96d7e80256f2be1
Aavesh Mistry <aavesh.mistry@gmail.com>
2019-04-03 12:04:18 +1100

    Initial HealthBar

---
+++ Assets/Scritps/Health/HealthBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HealthBar
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [Header("Reference to health")]
+        // Players maxium health
+        public float maxHealth;
+        // Players current health
+        public float curHealth;
+        [Header("Reference to UI elements")]
+        // Reference to Slider
+        public Slider healthSlider;
+        // Reference to Fill
+        public Image healthFill;
+
+        void Update()
+        {
+            healthSlider.value = Mathf.Clamp01(curHealth / maxHealth);
+            if (curHealth <= 0 && healthFill.enabled)
+            {
+                Debug.Log("Dead");
+                healthFill.enabled = false;
+            }
+            else if (!healthFill.enabled && curHealth > 0)
+            {
+                Debug.Log("Revive");
+                healthFill.enabled = enabled;
+            }
+        }
+    }
+}
